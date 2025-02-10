56b00e66cacda841d5f6ba93335f3f875498151a
Baymax-484 <hiro364896219@gmail.com>
2025-02-10 17:45:01 +0530

    name added

--- doubt.js
+++ doubt.js
@@ -1,32 +1,33 @@
-// const names=['john','doe','elon','musk']
-// // console.log(names.indexOf('elon'))
-// function checkValueInArray( value,array,index) {
-//     // return array.indexOf(value);
-//     return value === array[index];
-// }
+const names=['john','doe','elon','musk']
+// console.log(names.indexOf('elon'))
+function checkValueInArray( value,array,index) {
+    // return array.indexOf(value);
+    return value === array[index];
+}
 
-// let y=checkValueInArray('elon',names, 2)
-// console.log(y)
+let y=checkValueInArray('elon',names, 2)
+console.log(y)
 
-// const shoppingCart = [
-//     {name: 'Apple', price: 1.99, quantity: 3},
-//     {name: 'Apple', price: 1.99, quantity: 3},
-//     {name: 'Apple', price: 1.99, quantity: 3},
-//     {name: 'Xiomi', price: 2.99, quantity: 2},
-//     {name: 'Samsung', price: 3.99, quantity: 1},
-//     {name: 'Tesla', price: 3.99, quantity: 1},
-//     {name: 'Tesla', price: 4.99, quantity: 4},
-//     {name: 'Nokia', price: 4.99, quantity: 4},
-// ]
+const shoppingCart = [
+    {name: 'Apple', price: 1.99, quantity: 3},
+    {name: 'Apple', price: 1.99, quantity: 3},
+    {name: 'Apple', price: 1.99, quantity: 3},
+    {name: 'Xiomi', price: 2.99, quantity: 2},
+    {name: 'Samsung', price: 3.99, quantity: 1},
+    {name: 'Tesla', price: 3.99, quantity: 1},
+    {name: 'Tesla', price: 4.99, quantity: 4},
+    {name: 'Nokia', price: 4.99, quantity: 4},
+]
 
-// const products = shoppingCart.reduce((productGroup, product) => {
-//     const name = product.name;
-//     if(productGroup[name] == null) {
-//         productGroup[name] = [];
-//     }
-//     productGroup[name].push(product);
+const products = shoppingCart.reduce((productGroup, product) => {
+    const name = product.name;
+    if(productGroup[name] == null) {
+        productGroup[name] = [];
+    }
+    productGroup[name].push(product);
 
-//     return productGroup;
-// }, {});
+    return productGroup;
+}, {});
 
-// console.log(products);
+console.log(products);
+console.log(name);
